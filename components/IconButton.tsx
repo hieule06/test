09b18fdf9ce18a81d85c85c7b741cbc75fb817d0
Bien Le <bienle160720@gmail.com>
2024-03-06 23:51:55 +0700

    Revert test

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -7,7 +7,6 @@ const IconButton = ({ icon: Icon, label }: Props) => {
 	return (
 		<button className='flex items-center space-x-2 hover:text-white'>
 			<Icon className='icon' />
-			44444
 			<span>{label}</span>
 		</button>
 	)
